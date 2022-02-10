e26a26df455a94e0c9c1634bd7b381a226d10da2
BethaSL <bethasanchez3@gmail.com>
2022-02-10 15:09:50 +0000

    Changed models.py

--- src/models.py
+++ src/models.py
@@ -8,26 +8,49 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User (Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    email = Column(String(220), nullable=False)
+    password = Column(String(10), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    favorites= relationship("Favorites", back_populates="parent")
+
+
+class Favorites (Base):
+    __tablename__ = 'favorites'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id= Column(Integer, ForeignKey("user.id"))
+    
+    user= relationship("User", back_populates="children")
+    planets= relationship("Planets", back_populates="parent")
+    characters= relationship("Characters", back_populates="parent")
+
+
+class Planets (Base):
+    __tablename__ = 'planets'
+    id = Column(String, primary_key=True)
+    name_planet = Column(String, nullable=False)
+    population = Column(Integer, nullable=False)
+    review_planet = Column(String, nullable=False)
+    favorites_id= Column(Integer, ForeignKey("favorites.id"))
+
+    favorites= relationship("Favorites", back_populates="children")
+
+
+class Characters (Base):
+    __tablename__ = 'characters'
+    id = Column(String, primary_key=True)
+    name_character = Column(String, nullable=False)
+    gender = Column(String, nullable=False)
+    review_character = Column(String, nullable=False)
+    favorites_id= Column(Integer, ForeignKey("favorites.id"))
+
+    favorites= relationship("Favorites", back_populates="children")
 
     def to_dict(self):
         return {}
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
